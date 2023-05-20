f8f6c1e1c304cba910e4dcec699250b5ff98eb40
Anujan Asokkumar <anujan.asokkumar@gmail.com>
2023-05-20 16:44:04 -0700

    merging pdf to llama

---
+++ basicLLM.py
@@ -0,0 +1,6 @@
+from langchain.agents import agent
+from langchain.tools import Tool
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+
--- pythonPDF.py
+++ pythonPDF.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import PyPDF2
 import os
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+
 
 def convert_pdf_to_text(file):
     # Read the PDF file
@@ -15,8 +17,32 @@ def convert_pdf_to_text(file):
     return text
 
 def save_text_file(text, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
+    folder_path = "./data"  # Specify the folder path where you want to save the file
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'w', encoding='utf-8') as file:
         file.write(text)
+
+# Example usage
+text = "This is some sample text."
+filename = "output.txt"
+save_text_file(text, filename)
+
+def ask_llama():
+    documents = SimpleDirectoryReader('data').load_data()
+    index = GPTVectorStoreIndex.from_documents(documents)
+
+    query_engine = index.as_query_engine()
+    questions = ["Summarize the text",
+             "Highlight key legal points",
+             "Translate for the average user to read it",
+             ]
+
+    for question in questions:
+        print(question)
+        response = query_engine.query(question)
+        st.write(response)
 
 def main():
     st.title("PDF to Text Converter")
@@ -37,6 +63,6 @@ def main():
         text_filename = base_filename + ".txt"
         save_text_file(text, text_filename)
         st.success("Text file saved as " + text_filename)
-
+        ask_llama()
 if __name__ == '__main__':
     main()
